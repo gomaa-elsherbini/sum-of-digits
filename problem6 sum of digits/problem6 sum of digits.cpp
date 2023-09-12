147fad77c4460b651cb408d4e11b4fcf0ecb8543
gomaa <gogochemist35@gmail.com>
2023-09-13 00:40:46 +0300

    another code

--- problem6 sum of digits/problem6 sum of digits.cpp
+++ problem6 sum of digits/problem6 sum of digits.cpp
@@ -11,54 +11,75 @@ int readNumber()
 	return num;
 }
 
-int getLength(string str)
+int sumDigitsOfNumber(int num)
 {
-	int i = 0;
-	int length = 0;
-
-	while (str[i])
-	{
-		if (str[i] == '-')
-		{
-			i++;
-			continue;
-		}
-		length += 1;
-		i++;
-	}
-	return length;
-}
-
-int sumReversedOrderDigits(int num)
-{
+	int Reminder = 0;
 	int sum = 0;
 
-	string str = to_string(num);
-
-	int length = getLength(str);
-
-	for (int i =0; i <length; i++)
+	while (num>0)
 	{
-		sum += str[i] - '0';
+		Reminder = num % 10;
+		num = num / 10;
+
+		sum += Reminder;
 	}
 
 	return sum;
 }
 
-int sumNumberDigits(int num)
-{
-	int sum = 0;
 
-	for (int i = 1; i <= num; i++)
-	{
-		sum += i;
-	}
-	return sum;
-}
+
 
 int main()
 {
-	cout << sumReversedOrderDigits(readNumber()) << endl;
+	cout << sumDigitsOfNumber(readNumber()) << endl;
 
 	return 0;
 }
+
+
+
+//int getLength(string str)
+//{
+//	int i = 0;
+//	int length = 0;
+//
+//	while (str[i])
+//	{
+//		if (str[i] == '-')
+//		{
+//			i++;
+//			continue;
+//		}
+//		length += 1;
+//		i++;
+//	}
+//	return length;
+//}
+//
+//int sumReversedOrderDigits(int num)
+//{
+//	int sum = 0;
+//
+//	string str = to_string(num);
+//
+//	int length = getLength(str);
+//
+//	for (int i =0; i <length; i++)
+//	{
+//		sum += str[i] - '0';
+//	}
+//
+//	return sum;
+//}
+//
+//int sumNumberDigits(int num)
+//{
+//	int sum = 0;
+//
+//	for (int i = 1; i <= num; i++)
+//	{
+//		sum += i;
+//	}
+//	return sum;
+//}
